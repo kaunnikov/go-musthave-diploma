2aff3e9720618d1b20ccaffdadf9cea67eab1068
Alexander <kaun-aleksandr@yandex.ru>
2023-08-31 12:32:17 +0500

    Sprint 5

--- internal/accrual/accrual.go
+++ internal/accrual/accrual.go
@@ -32,66 +32,62 @@ func CheckOrders(accrualURL string) {
 	ch := generator(numbers)
 
 	for number := range ch {
-		// Отправляем запрос в систему расчёта баллов
-		resp, err := fetch(accrualURL + strconv.FormatInt(number, 10))
-		if err != nil {
-			logging.Errorf("Ошибка отправки запроса в систему расчёта баллов: %s", err)
-			continue
-		}
-
-		body, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			logging.Errorf("cannot read request body: %s", err)
-			return
-		}
-
-		// Заказа нет в системе - убираем в Invalid
-		switch resp.StatusCode {
-		case http.StatusNoContent:
-			if err = services.SetInvalidStatusByNumber(number); err != nil {
-				logging.Errorf("Не смогли установить статуст Invalid: %s", err)
-			}
-		case http.StatusOK:
-			var s RespAPI
-			err = json.Unmarshal(body, &s)
-			if err != nil {
-				logging.Errorf("cannot decode response: %s", err)
-				return
-			}
-
-			switch s.Status {
-			case "INVALID":
-				if err = services.SetInvalidStatusByNumber(number); err != nil {
-					logging.Errorf("Не смогли установить статуст Invalid: %s", err)
-				}
-			case "PROCESSED":
-				if err = services.SetProcessedStatusByNumber(number, int(s.Accrual*100)); err != nil {
-					logging.Errorf("Не смогли установить статуст Processed: %s", err)
-				}
-			default:
-				// Выставляем обратно статус Processing, чтобы взять в работу снова через N времени
-				if err = services.SetProcessingStatusByNumber(number); err != nil {
-					logging.Errorf("Не смогли установить статуст Processing: %s", err)
-				}
-			}
-		}
+		checkOrder(accrualURL, number)
 	}
 }
 
-func fetch(URL string) (*http.Response, error) {
-	resp, err := http.Get(URL)
-	if err != nil {
-		return nil, err
-	}
+func checkOrder(accrualURL string, number int64) {
 
+	// Отправляем запрос в систему расчёта баллов
+	resp, err := http.Get(accrualURL + strconv.FormatInt(number, 10))
+	if err != nil {
+		logging.Errorf("Ошибка отправки запроса в систему расчёта баллов: %s", err)
+		return
+	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 			logging.Errorf("don't close body: %s", err)
 		}
 	}(resp.Body)
-	return resp, err
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		logging.Errorf("cannot read request body: %s", err)
+		return
+	}
+
+	// Заказа нет в системе - убираем в Invalid
+	switch resp.StatusCode {
+	case http.StatusNoContent:
+		if err = services.SetInvalidStatusByNumber(number); err != nil {
+			logging.Errorf("Не смогли установить статуст Invalid: %s", err)
+		}
+	case http.StatusOK:
+		var s RespAPI
+		err = json.Unmarshal(body, &s)
+		if err != nil {
+			logging.Errorf("cannot decode response: %s", err)
+			return
+		}
+
+		switch s.Status {
+		case "INVALID":
+			if err = services.SetInvalidStatusByNumber(number); err != nil {
+				logging.Errorf("Не смогли установить статуст Invalid: %s", err)
+			}
+		case "PROCESSED":
+			if err = services.SetProcessedStatusByNumber(number, int(s.Accrual*100)); err != nil {
+				logging.Errorf("Не смогли установить статуст Processed: %s", err)
+			}
+		default:
+			// Выставляем обратно статус Processing, чтобы взять в работу снова через N времени
+			if err = services.SetProcessingStatusByNumber(number); err != nil {
+				logging.Errorf("Не смогли установить статуст Processing: %s", err)
+			}
+		}
+	}
 }
 
 func generator(numbers []int64) chan int64 {
